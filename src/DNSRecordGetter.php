a27cae1882c31c219e2ae05b438156bc0812af58
Mikael Peigney <mikael.peigney@gamersflag.com>
2017-08-11 16:28:01 +0200

    Restore DNSRecordGetter with spaces instead of tabs

--- src/DNSRecordGetter.php
+++ src/DNSRecordGetter.php
@@ -12,109 +12,109 @@ use Mika56\SPFCheck\Exception\DNSLookupLimitReachedException;
 
 class DNSRecordGetter implements DNSRecordGetterInterface
 {
-	protected $requestCount = 0;
+    protected $requestCount = 0;
 
-	/**
-	 * @param $domain string The domain to get SPF record
-	 * @return string[] The SPF record(s)
-	 * @throws DNSLookupException
-	 */
-	public function getSPFRecordForDomain($domain)
-	{
-		$records = dns_get_record($domain, DNS_TXT | DNS_SOA);
-		if (false === $records) {
-			throw new DNSLookupException;
-		}
+    /**
+     * @param $domain string The domain to get SPF record
+     * @return string[] The SPF record(s)
+     * @throws DNSLookupException
+     */
+    public function getSPFRecordForDomain($domain)
+    {
+        $records = dns_get_record($domain, DNS_TXT | DNS_SOA);
+        if (false === $records) {
+            throw new DNSLookupException;
+        }
 
-		$spfRecords = array();
-		foreach ($records as $record) {
-			if ($record['type'] == 'TXT') {
-				$txt = strtolower($record['txt']);
-				// An SPF record can be empty (no mechanism)
-				if ($txt == 'v=spf1' || stripos($txt, 'v=spf1 ') === 0) {
-					$spfRecords[] = $txt;
-				}
-			}
-		}
+        $spfRecords = array();
+        foreach ($records as $record) {
+            if ($record['type'] == 'TXT') {
+                $txt = strtolower($record['txt']);
+                // An SPF record can be empty (no mechanism)
+                if ($txt == 'v=spf1' || stripos($txt, 'v=spf1 ') === 0) {
+                    $spfRecords[] = $txt;
+                }
+            }
+        }
 
-		return $spfRecords;
-	}
+        return $spfRecords;
+    }
 
-	public function resolveA($domain, $ip4only = false)
-	{
-		$records = dns_get_record($domain, $ip4only ? DNS_A : (DNS_A | DNS_AAAA));
-		if (false === $records) {
-			throw new DNSLookupException;
-		}
+    public function resolveA($domain, $ip4only = false)
+    {
+        $records = dns_get_record($domain, $ip4only ? DNS_A : (DNS_A | DNS_AAAA));
+        if (false === $records) {
+            throw new DNSLookupException;
+        }
 
-		$addresses = [];
+        $addresses = [];
 
-		foreach ($records as $record) {
-			if ($record['type'] === "A") {
-				$addresses[] = $record['ip'];
-			} elseif ($record['type'] === 'AAAA') {
-				$addresses[] = $record['ipv6'];
-			}
-		}
+        foreach ($records as $record) {
+            if ($record['type'] === "A") {
+                $addresses[] = $record['ip'];
+            } elseif ($record['type'] === 'AAAA') {
+                $addresses[] = $record['ipv6'];
+            }
+        }
 
-		return $addresses;
-	}
+        return $addresses;
+    }
 
-	public function resolveMx($domain)
-	{
-		$records = dns_get_record($domain, DNS_MX);
-		if (false === $records) {
-			throw new DNSLookupException;
-		}
+    public function resolveMx($domain)
+    {
+        $records = dns_get_record($domain, DNS_MX);
+        if (false === $records) {
+            throw new DNSLookupException;
+        }
 
-		$addresses = [];
+        $addresses = [];
 
-		foreach ($records as $record) {
-			if ($record['type'] === "MX") {
-				$addresses[] = $record['target'];
-			}
-		}
+        foreach ($records as $record) {
+            if ($record['type'] === "MX") {
+                $addresses[] = $record['target'];
+            }
+        }
 
-		return $addresses;
-	}
+        return $addresses;
+    }
 
-	public function resolvePtr($ipAddress)
-	{
-		if (stripos($ipAddress, '.') !== false) {
-			// IPv4
-			$revIp = implode('.', array_reverse(explode('.', $ipAddress))).'.in-addr.arpa';
-		} else {
-			$literal = implode(':', array_map(function ($b) {
-				return sprintf('%04x', $b);
-			}, unpack('n*', inet_pton($ipAddress))));
-			$revIp   = strtolower(implode('.', array_reverse(str_split(str_replace(':', '', $literal))))).'.ip6.arpa';
-		}
+    public function resolvePtr($ipAddress)
+    {
+        if (stripos($ipAddress, '.') !== false) {
+            // IPv4
+            $revIp = implode('.', array_reverse(explode('.', $ipAddress))).'.in-addr.arpa';
+        } else {
+            $literal = implode(':', array_map(function ($b) {
+                return sprintf('%04x', $b);
+            }, unpack('n*', inet_pton($ipAddress))));
+            $revIp   = strtolower(implode('.', array_reverse(str_split(str_replace(':', '', $literal))))).'.ip6.arpa';
+        }
 
-		$revs = array_map(function ($e) {
-			return $e['target'];
-		}, dns_get_record($revIp, DNS_PTR));
+        $revs = array_map(function ($e) {
+            return $e['target'];
+        }, dns_get_record($revIp, DNS_PTR));
 
-		return array_slice($revs, 0, 10);
-	}
+        return array_slice($revs, 0, 10);
+    }
 
-	public function exists($domain)
-	{
-		try {
-			return count($this->resolveA($domain, true)) > 0;
-		} catch (DNSLookupException $e) {
-			return false;
-		}
-	}
+    public function exists($domain)
+    {
+        try {
+            return count($this->resolveA($domain, true)) > 0;
+        } catch (DNSLookupException $e) {
+            return false;
+        }
+    }
 
-	public function resetRequestCount()
-	{
-		$this->requestCount = 0;
-	}
+    public function resetRequestCount()
+    {
+        $this->requestCount = 0;
+    }
 
-	public function countRequest()
-	{
-		if (++$this->requestCount > 10) {
-			throw new DNSLookupLimitReachedException();
-		}
-	}
+    public function countRequest()
+    {
+        if (++$this->requestCount > 10) {
+            throw new DNSLookupLimitReachedException();
+        }
+    }
 }
